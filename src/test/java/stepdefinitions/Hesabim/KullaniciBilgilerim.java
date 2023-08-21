9a3b93d0e765da1d52d4bfc7bcc46c2231b45a29
Selma0204 <selmaeren.0204@gmail.com>
2023-08-21 17:50:26 +0300

    selma

--- src/test/java/stepdefinitions/Hesabim/KullaniciBilgilerim.java
+++ src/test/java/stepdefinitions/Hesabim/KullaniciBilgilerim.java
@@ -1,4 +1,41 @@
 package stepdefinitions.Hesabim;
 
+import io.cucumber.java.en.And;
+
 public class KullaniciBilgilerim {
+    @And("click on Hesabim")
+    public void clickOnHesabim() {
+    }
+
+    @And("click on Hesap Ayarlarim")
+    public void clickOnHesapAyarlarim() {
+    }
+
+    @And("click on Kullanici Bilgilerim")
+    public void clickOnKullaniciBilgilerim() {
+    }
+
+    @And("click on the {string} section")
+    public void clickOnTheAdSoyadSection() {
+    }
+
+    @And("enter a valid value in the {string} field")
+    public void enterAValidValueInTheAdSoyadField() {
+    }
+
+    @And("see registered e-mail address")
+    public void seeRegisteredEMailAddress() {
+    }
+
+    @And("click on mobile phone section")
+    public void clickOnMobilePhoneSection() {
+    }
+
+    @And("enter valid phone number")
+    public void enterValidPhoneNumber() {
+    }
+
+    @And("click the Güncelle button")
+    public void clickTheGuncelleButton() {
+    }
 }
--- src/test/resources/features/Hesabim/KullaniciBilgilerim.feature
+++ src/test/resources/features/Hesabim/KullaniciBilgilerim.feature
@@ -1,1 +1,18 @@
-Feature: Name Last Name
+Feature: Kullanici Bilgilerim
+
+  Scenario: Kullanici Bilgilerim Test
+
+   Given Trendyol mobile application is launched
+   When Login is done.
+   And click on Hesabim
+   And click on Hesap Ayarlarim
+   And click on Kullanici Bilgilerim
+   And click on the 'Ad Soyad' section
+   And enter a valid value in the 'Ad Soyad' field
+   And see registered e-mail address
+   And click on mobile phone section
+   And enter valid phone number
+   And click the Güncelle button
+
+
+
